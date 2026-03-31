119cf1b359aa16f29c711d8840ba2c9dfd03e194
mv24csb0b42 <mv24csb0b42@student.nitw.ac.in>
2026-03-31 16:04:38 +0530

    Add files via upload

--- explaination_model.py
+++ explaination_model.py
@@ -9,7 +9,17 @@ from models.templates import (
     NAME_ERROR_TEMPLATE,
     SYNTAX_ERROR_TEMPLATE,
     TYPE_ERROR_TEMPLATE,
-    GENERIC_ERROR_TEMPLATE
+    GENERIC_ERROR_TEMPLATE,
+    INDEX_ERROR_TEMPLATE,
+    KEY_ERROR_TEMPLATE,
+    ATTRIBUTE_ERROR_TEMPLATE,
+    VALUE_ERROR_TEMPLATE,
+    IMPORT_ERROR_TEMPLATE,
+    FILE_NOT_FOUND_TEMPLATE,
+    ZERO_DIVISION_TEMPLATE,
+    INDENTATION_ERROR_TEMPLATE,
+    MODULE_NOT_FOUND_TEMPLATE,
+    OVERFLOW_ERROR_TEMPLATE
 )
 
 def explain_error(error, code):
@@ -58,7 +68,48 @@ def generate_explanation(error_message, ast_context):
     elif "typeerror" in error_message or error_type == "TypeError":
         return TYPE_ERROR_TEMPLATE.format(line=line)
 
+    # -------- ZeroDivisionError --------
+    elif "zerodivisionerror" in error_message:
+       return ZERO_DIVISION_TEMPLATE.format(line=line)
+
+    # -------- IndexError --------
+    elif "indexerror" in error_message:
+      return INDEX_ERROR_TEMPLATE.format(line=line)
+
+    # -------- KeyError --------
+     elif "keyerror" in error_message:
+      return KEY_ERROR_TEMPLATE.format(line=line)
+
+     # -------- AttributeError --------
+      elif "attributeerror" in error_message:
+       return ATTRIBUTE_ERROR_TEMPLATE.format(line=line)
+
+     # -------- ValueError --------
+      elif "valueerror" in error_message:
+       return VALUE_ERROR_TEMPLATE.format(line=line)
+
+     # -------- ImportError --------
+       elif "importerror" in error_message:
+       return IMPORT_ERROR_TEMPLATE.format(line=line)
+
+     # -------- FileNotFoundError --------
+       elif "filenotfounderror" in error_message:
+        return FILE_NOT_FOUND_TEMPLATE.format(line=line)
+
+    # -------- IndentationError --------
+      elif "indentationerror" in error_message:
+       return INDENTATION_ERROR_TEMPLATE.format(line=line)
+
+     # -------- ModuleNotFoundError --------
+      elif "modulenotfounderror" in error_message:
+        return MODULE_NOT_FOUND_TEMPLATE.format(line=line)
+
+    # -------- OverflowError --------
+       elif "overflowerror" in error_message:
+       return OVERFLOW_ERROR_TEMPLATE.format(line=line)
+
+
+
     # -------- Fallback --------
     else:
         return GENERIC_ERROR_TEMPLATE.format(line=line)
-
--- templates.py
+++ templates.py
@@ -27,3 +27,46 @@ GENERIC_ERROR_TEMPLATE = (
     "An error occurred on line {line}. "
     "Please review the code near this line for possible mistakes."
 )
+
+# Additional Error Templates
+
+INDEX_ERROR_TEMPLATE = (
+    "You are trying to access an index that is out of range on line {line}. "
+    "Make sure the index exists in the list."
+)
+
+KEY_ERROR_TEMPLATE = (
+    "The key you are trying to access does not exist in the dictionary on line {line}."
+)
+
+ATTRIBUTE_ERROR_TEMPLATE = (
+    "The object does not have the attribute or method you are trying to use on line {line}."
+)
+
+VALUE_ERROR_TEMPLATE = (
+    "An invalid value is used in an operation on line {line}."
+)
+
+IMPORT_ERROR_TEMPLATE = (
+    "The module you are trying to import cannot be found on line {line}."
+)
+
+FILE_NOT_FOUND_TEMPLATE = (
+    "The file you are trying to access does not exist on line {line}."
+)
+
+ZERO_DIVISION_TEMPLATE = (
+    "Division by zero is not allowed on line {line}."
+)
+
+INDENTATION_ERROR_TEMPLATE = (
+    "There is an indentation issue in your code on line {line}. Check spacing and alignment."
+)
+
+MODULE_NOT_FOUND_TEMPLATE = (
+    "The specified module could not be found on line {line}."
+)
+
+OVERFLOW_ERROR_TEMPLATE = (
+    "A numerical operation exceeded the allowed limit on line {line}."
+)
